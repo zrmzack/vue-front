cd35a6c1165c662df9e6a95edbd0e2bcfaa388ff
zhangrunmin <>
2021-12-17 08:58:49 +0800

    index

---
+++ src/views/redis/importConfig.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios'
+
+// eslint-disable-next-line no-unused-vars
+const request = axios.create({
+  baseURL: 'http://127.0.0.1:9998/'
+})
+// eslint-disable-next-line no-undef
+export default request
---
+++ src/views/redis/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <el-form>
+      <textarea v-model="key_value" placeholder="请输入问题" type="text"/>
+      <textarea v-model="value_value" placeholder="请输入答案" type="text"/>
+      <button @click="oncreate()">提交</button>
+    </el-form>
+  </div>
+</template>
+<script>
+// eslint-disable-next-line no-unused-vars
+import request from '@/views/redis/importConfig'
+
+export default {
+  name: 'Redis',
+  data: function() {
+    return {
+      key_value: '',
+      value_value: ''
+    }
+  },
+  methods: {
+    oncreate() {
+      var tempx = this.key_value
+      var tempy = this.value_value
+      var newurl = '/redis/add/' + tempx.toString() + '/' + tempy.toString()
+      console.log(newurl)
+      request({
+        method: 'POST',
+        url: newurl
+      })
+      alert('redis存储成功')
+    }
+
+  }
+}
+</script>
